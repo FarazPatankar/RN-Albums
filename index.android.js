d9eefdcbb847b746e8289a895bc73f4934ee4073
Faraz Patankar <farazpatankar@gmail.com>
2016-11-16 23:53:17 +0530

    make header component dynamic/reusable using props

--- index.android.js
+++ index.android.js
@@ -1,12 +1,12 @@
 // Importing the library to help create a component
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { AppRegistry } from 'react-native';
 import Header from './src/components/header';
 
 // Create a component
 const App = () => {
 	return (
-		<Header />
+		<Header headerText={'Albums'} />
 	);
 };
 
