7082a006733dfc80d0dcc05b2c0f7ac11fb3cca5
Faraz Patankar <farazpatankar@gmail.com>
2016-11-16 23:18:07 +0530

    create, import, nest and render header component

--- index.android.js
+++ index.android.js
@@ -1,11 +1,12 @@
 // Importing the library to help create a component
 import React from 'react';
 import { Text, AppRegistry } from 'react-native';
+import Header from './src/components/header';
 
 // Create a component
 const App = () => {
   return (
-    <Text>Some Text</Text>
+    <Header />
   );
 };
 
